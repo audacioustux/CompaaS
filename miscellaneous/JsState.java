b838f462d5dc3024df5adbda889c996ee5ed4762
Tanjim Hossain <tangimhossain1@gmail.com>
2023-02-13 19:45:31 +0000

    ..
    Former-commit-id: 06f5ca9297e69976a8af5b314519082f110ea46c

--- miscellaneous/JsState.java
+++ miscellaneous/JsState.java
@@ -7,31 +7,82 @@ import org.graalvm.polyglot.proxy.*;
 import java.util.*;
 
 class Main {
-	public static void main(String[] args) throws IOException {
-		Engine engine = Engine.newBuilder().build();
-		Context context = Context.newBuilder().allowExperimentalOptions(true)
-				.option("js.esm-eval-returns-exports", "true").engine(engine).build();
-
-		ProxyObject state = ProxyObject.fromMap(new HashMap<String, Object>() {
+	public static void main(String[] args) throws IOException, NoSuchMethodException {
+		Map<String, Object> state = new HashMap<String, Object>() {
 			{
 				put("count", 42);
 			}
-		});
+		};
 
 		String code = """
-				let state;
-				export const init = (s) => { state = s; }
-				export const increment = () => state.count++;
+				export const increment = (state) => { state.count++; };
+				export const add_member = (state) => { state.foo = 'bar'; };
+				export const add_closure = (state) => () => { state.count++; };
+
+				class Rectangle {
+					constructor(height, width) {
+						this.height = height;
+						this.width = width;
+					}
+					// Getter
+					get area() {
+						return this.calcArea();
+					}
+					// Method
+					calcArea() {
+						return this.height * this.width;
+					}
+					// Generator
+					*getSides() {
+						yield this.height;
+						yield this.width;
+						yield this.height;
+						yield this.width;
+					}
+				}
+
+				export const add_rect = (state) => state.rect = new Rectangle(10, 10);
+				export const get_rect_area = (state) => state.rect.calcArea();
 				""";
 
 		Source source = Source.newBuilder("js", code, "test.mjs").mimeType("application/javascript+module").build();
 
-		Value exports = context.eval(source);
+		Engine engine = Engine.newBuilder().build();
 
-		exports.invokeMember("init", state);
-		exports.invokeMember("increment");
+		HostAccess.Builder builder = HostAccess.newBuilder();
+		// https://github.com/oracle/graaljs/issues/143
+		builder.allowMapAccess(true);
 
-		Object count = state.getMember("count");
-		System.out.println("count: " + count);
+		{
+			Context context = Context.newBuilder().allowExperimentalOptions(true)
+					.option("js.esm-eval-returns-exports", "true").allowHostAccess(builder.build()).engine(engine)
+					.build();
+
+			Value exports = context.eval(source);
+
+			exports.invokeMember("increment", state);
+			System.out.println("count: " + state.get("count"));
+
+			exports.invokeMember("add_member", state);
+			System.out.println("foo: " + state.get("foo"));
+
+			exports.invokeMember("add_closure", state).execute();
+			System.out.println("count: " + state.get("count"));
+
+			exports.invokeMember("add_rect", state);
+			Value rect_area = exports.invokeMember("get_rect_area", state);
+			System.out.println("rect area: " + rect_area);
+
+			// context.close();
+		}
+
+		try (Context context = Context.newBuilder().allowExperimentalOptions(true)
+				.option("js.esm-eval-returns-exports", "true").allowHostAccess(builder.build()).engine(engine)
+				.build()) {
+			Value exports = context.eval(source);
+
+			Value rect_area = exports.invokeMember("get_rect_area", state);
+			System.out.println("rect area: " + rect_area);
+		}
 	}
 }
