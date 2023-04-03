8049c883c1d0411dc2ba78d4c646fb8dff6f19aa
Tanjim Hossain <tangimhossain1@gmail.com>
2023-04-03 08:12:08 +0000

    clean dockerfile
    Former-commit-id: abe4b13820839773a32323e9b5eab6ff8d1a54c6

---
+++ .devcontainer/scripts/post-create.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env zsh
+
+install_sdkman() {
+    curl -s "https://get.sdkman.io" | bash
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
+    sdk version
+}
+
+install_graalvm() {
+    GRAALVM_EDITION=${GRAALVM_EDITION:-ce}
+    GRAALVM_VERSION=${GRAALVM_VERSION:-"22.3.1"}
+    GRAALVM_COMPONENTS=${GRAALVM_COMPONENTS:-"native-image,nodejs,wasm"}
+    JAVA_VERSION=${JAVA_VERSION:-"java17"}
+
+    GRAALVM_NAME=graalvm-${GRAALVM_EDITION}-${JAVA_VERSION}-${GRAALVM_VERSION}
+
+    curl -sL https://get.graalvm.org/jdk | sudo bash -s -- $GRAALVM_NAME \
+        --to /opt \
+        -c $GRAALVM_COMPONENTS
+
+    SDK_GRAALVM_ID=${GRAALVM_VERSION:0:6}.r${JAVA_VERSION:4:2}-grl${GRAALVM_EDITION:0:1}
+    sdk install java ${SDK_GRAALVM_ID} ${GRAALVM_HOME}
+    sdk default java ${SDK_GRAALVM_ID}
+
+    java -version
+}
+
+install_sbt() {
+    sdk install sbt
+}
+
+install_sdkman
+install_graalvm
+install_sbt
