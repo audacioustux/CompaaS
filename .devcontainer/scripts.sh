7fe1e8fff8cf2db8bf4e104378518e5e089e7ce8
Tanjim Hossain <tangimhossain1@gmail.com>
2023-04-03 15:47:03 +0000

    parallelize postCreateCommand
    Former-commit-id: 9a016e4d61ba16ea2957fd1743d85dec460b4996

--- .devcontainer/scripts.sh
+++ .devcontainer/scripts.sh
@@ -3,37 +3,40 @@
 install_sdkman() {
     curl -s "https://get.sdkman.io" | bash
     source "$HOME/.sdkman/bin/sdkman-init.sh"
-    sdk version
 }
 
 install_graalvm() {
     GRAALVM_EDITION=${GRAALVM_EDITION:-ce}
     GRAALVM_VERSION=${GRAALVM_VERSION:-"22.3.1"}
-    GRAALVM_COMPONENTS=${GRAALVM_COMPONENTS:-"native-image,nodejs,wasm"}
     JAVA_VERSION=${JAVA_VERSION:-"java17"}
 
     GRAALVM_NAME=graalvm-${GRAALVM_EDITION}-${JAVA_VERSION}-${GRAALVM_VERSION}
 
     curl -sL https://get.graalvm.org/jdk | sudo bash -s -- $GRAALVM_NAME \
-        --to /opt \
-        -c $GRAALVM_COMPONENTS
+        --to $HOME/ 
 
-    GRAALVM_HOME="/opt/${GRAALVM_NAME}"
+    GRAALVM_HOME="$HOME/${GRAALVM_NAME}"
 
     SDK_GRAALVM_ID=${GRAALVM_VERSION:0:6}.r${JAVA_VERSION:4:2}-grl${GRAALVM_EDITION:0:1}
     sdk install java ${SDK_GRAALVM_ID} ${GRAALVM_HOME}
     sdk default java ${SDK_GRAALVM_ID}
 
-    sdk current java
+    export PATH=${GRAALVM_HOME}/bin:$PATH
+    export JAVA_HOME=${GRAALVM_HOME}
+}
+
+install_graalvm_components(){
+    GRAALVM_COMPONENTS=${GRAALVM_COMPONENTS:-"native-image,nodejs,wasm"}
+    for component in ${GRAALVM_COMPONENTS//,/ }; do
+        gu install ${component}
+    done
 }
 
 install_sbt() {
     sdk install sbt
-
-    sdk current sbt
 }
 
-create_k8s_cluster() {
+create_minikube_cluster() {
     minikube start --cpus 2 --memory 4096 --driver=docker
 }
 
@@ -52,18 +55,3 @@ apply_k8s_manifests() {
 install_k9s() {
     curl -sS https://webi.sh/k9s | sh
 }
-
-cleanup() {
-    apt-get autoremove -y
-    apt-get clean -y
-}
-
-install_sdkman
-install_graalvm
-install_sbt
-create_k8s_cluster
-minikube_docker_env
-publishLocalImage
-apply_k8s_manifests
-install_k9s
-cleanup
