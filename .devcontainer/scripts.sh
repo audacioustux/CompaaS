000541105825b22b16b6f04d78459718febde7e1
Tanjim Hossain <tangimhossain1@gmail.com>
2023-04-03 23:25:14 +0600

    Update scripts.sh
    Former-commit-id: 1563838563608afa109fd8904549f86342f72b37

--- .devcontainer/scripts.sh
+++ .devcontainer/scripts.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 install_sdkman() {
     curl -s "https://get.sdkman.io" | bash
     source "$HOME/.sdkman/bin/sdkman-init.sh"
@@ -9,11 +11,13 @@ install_graalvm() {
     GRAALVM_EDITION=${GRAALVM_EDITION:-ce}
     GRAALVM_VERSION=${GRAALVM_VERSION:-"22.3.1"}
     JAVA_VERSION=${JAVA_VERSION:-"java17"}
+    GRAALVM_COMPONENTS=${GRAALVM_COMPONENTS:-"native-image,nodejs,wasm"}
 
     GRAALVM_NAME=graalvm-${GRAALVM_EDITION}-${JAVA_VERSION}-${GRAALVM_VERSION}
 
     curl -sL https://get.graalvm.org/jdk | sudo bash -s -- $GRAALVM_NAME \
-        --to $HOME/ 
+        --to $HOME/ \
+        -c $GRAALVM_COMPONENTS
 
     GRAALVM_HOME="$HOME/${GRAALVM_NAME}"
 
@@ -25,25 +29,14 @@ install_graalvm() {
     export JAVA_HOME=${GRAALVM_HOME}
 }
 
-install_graalvm_components(){
-    GRAALVM_COMPONENTS=${GRAALVM_COMPONENTS:-"native-image,nodejs,wasm"}
-    for component in ${GRAALVM_COMPONENTS//,/ }; do
-        gu install ${component}
-    done
-}
-
 install_sbt() {
     sdk install sbt
-}
-
-create_minikube_cluster() {
-    minikube start --cpus 2 --memory 4096 --driver=docker
 }
 
 minikube_docker_env() {
     echo "eval \$(minikube docker-env)" >> ~/.zshrc
 }
 
-install_k9s() {
-    curl -sS https://webi.sh/k9s | sh
-}
+install_sdkman
+install_graalvm
+install_sbt
