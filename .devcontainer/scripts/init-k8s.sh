f51362e098ac344b73dabfe43315f58210bebdff
Tanjim Hossain <tangimhossain1@gmail.com>
2023-04-04 13:03:12 +0600

    Update init-k8s.sh
    Former-commit-id: 4c5c6c53d9d09d8fdaaec49a636837463e42de60

--- .devcontainer/scripts/init-k8s.sh
+++ .devcontainer/scripts/init-k8s.sh
@@ -6,14 +6,9 @@ create_cluster(){
     minikube start
 }
 
-init_docker_env(){
-    echo "eval \$(minikube docker-env)" >> ~/.zshrc
-}
-
 install_k9s() {
     curl -sS https://webi.sh/k9s | sh
 }
 
 create_cluster
-init_docker_env
 install_k9s
