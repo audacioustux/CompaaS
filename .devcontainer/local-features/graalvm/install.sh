94709176473e8b25fcc546d53e087bb06fc9a130
Audacious Tux <tangimhossain1@gmail.com>
2023-06-01 16:53:49 +0600

    ..
    Former-commit-id: c2fb168a7cd80955fa208c20a54c1605df4b5300

--- .devcontainer/local-features/graalvm/install.sh
+++ .devcontainer/local-features/graalvm/install.sh
@@ -8,10 +8,9 @@ set -eax
 : ${GRAAL_COMPONENTS:="native-image,js,wasm"}
 
 GRAAL_RELEASE="graalvm-${GRAAL_EDITION}-java${JAVA_VERSION}-${GRAAL_VERSION}"
-echo "Installing GraalVM ${GRAAL_RELEASE}..."
 
 curl -sL https://get.graalvm.org/jdk | bash -s -- $GRAAL_RELEASE \
-    --to /opt \
+    --to /tmp \
     -c $GRAAL_COMPONENTS
 
-ln -s /opt/$GRAAL_RELEASE $JAVA_HOME
+mv /tmp/$GRAAL_RELEASE $JAVA_HOME
