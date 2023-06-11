371999a4b85638c13613e25bc6130940477ce903
Audacious Tux <tangimhossain1@gmail.com>
2023-06-11 09:20:19 +0000

    ..
    Former-commit-id: 81cc1366ab5e0258054443e95180d10b88a7873a

--- .devcontainer/local-features/sbt/install.sh
+++ .devcontainer/local-features/sbt/install.sh
@@ -2,17 +2,19 @@
 
 set -eax
 
-if [ "$(uname -m)" = "aarch64" ]; then
-    curl -fL "https://github.com/VirtusLab/coursier-m1/releases/latest/download/cs-aarch64-pc-linux.gz" | gzip -d > cs
-elif [ "$(uname -m)" = "x86_64" ]; then
+ARCH=$(uname -m)
+if [[ "$ARCH" == "x86_64" ]]; then
     curl -fL "https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz" | gzip -d > cs
+elif [[ "$ARCH" == "aarch64" ]]; then
+    curl -fL "https://github.com/VirtusLab/coursier-m1/releases/latest/download/cs-aarch64-pc-linux.gz" | gzip -d > cs
 else
     echo "Unsupported architecture: $(uname -m)"
     exit 1
 fi
 
 chmod +x cs
+install -o root -g root -m 0755 cs /usr/local/bin/cs
 
 sudo -iu $_REMOTE_USER <<EOF
-    ./cs setup
+    cs setup -y
 EOF
