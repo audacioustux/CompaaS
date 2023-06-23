8391bb64644d9a49a58989ff7b092c4267269e34
Audacious Tux <tangimhossain1@gmail.com>
2023-06-24 00:47:23 +0600

    ..

--- .devcontainer/features/common-utils-extras/install.sh
+++ .devcontainer/features/common-utils-extras/install.sh
@@ -4,16 +4,16 @@ set -eax
 
 apt-get update -qq
 apt-get install -yqq --no-install-recommends \
-    ripgrep \
-    bat \
-    exa \
-    delta \
-    hyperfine \
-    fd-find \
-    zoxide \
-    neovim \
-    python3-neovim \
-    httpie \
-    fzf \
-    socat \
+    # ripgrep \
+    # bat \
+    # exa \
+    # delta \
+    # hyperfine \
+    # fd-find \
+    # zoxide \
+    # neovim \
+    # python3-neovim \
+    # httpie \
+    # fzf \
+    # socat \
     parallel 
