6213060d79e0b41171d457bc915fef76b0983f62
Tanjim Hossain <tangimhossain1@gmail.com>
2023-06-24 16:19:39 +0000

    ..

--- .devcontainer/features/graalvm/install.sh
+++ .devcontainer/features/graalvm/install.sh
@@ -2,26 +2,24 @@
 
 set -eax
 
-JAVA_URL=https://download.oracle.com/java/${JAVA}/${VERSION}
-JAVA_HOME=/usr/java/jdk-${JAVA}
+URL_BASE=https://download.oracle.com/graalvm/${JAVA}/${VERSION}
+JDK_HOME=/usr/java/graal-${VERSION}-jdk${JAVA}
 
 ARCH="$(uname -m)"
 if [ "$ARCH" = "x86_64" ]; then
     ARCH="x64"
 fi
 
-JAVA_PKG="$JAVA_URL"/jdk-${JAVA}-"${ARCH}"_bin.tar.gz
+JAVA_PKG="$URL_BASE"/graalvm-jdk-${JAVA}_linux-"${ARCH}"_bin.tar.gz
 JAVA_SHA256=$(curl "$JAVA_PKG".sha256) 
 curl --output /tmp/jdk.tgz "$JAVA_PKG"
 echo "$JAVA_SHA256 */tmp/jdk.tgz" | sha256sum -c
-mkdir -p "$JAVA_HOME"
-tar --extract --file /tmp/jdk.tgz --directory "$JAVA_HOME" --strip-components 1
+mkdir -p "$JDK_HOME"
+tar --extract --file /tmp/jdk.tgz --directory "$JDK_HOME" --strip-components 1
 
-PATH=$JAVA_HOME/bin:$PATH
+mkdir -p "$JAVA_HOME"
+ln -s $JDK_HOME $JAVA_HOME
 
 if [[ -n "$COMPONENTS" ]]; then
     gu install $COMPONENTS
 fi
-
-mkdir -p "$GRAALVM_HOME"
-ln -s $JAVA_HOME $GRAALVM_HOME
