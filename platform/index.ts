60d34a06d976c25040d0aa11d9b6a335bde7f8a0
Audacious Tux <tangimhossain1@gmail.com>
2023-06-27 18:20:21 +0000

    add traefik

--- platform/index.ts
+++ platform/index.ts
@@ -1,6 +1,14 @@
 import * as k8s from "@pulumi/kubernetes";
 import { Namespace } from "@pulumi/kubernetes/core/v1";
 
+function useNamespace(namespace: Namespace) {
+    return (obj: any) => {
+        if (obj.metadata) {
+            obj.metadata.namespace = namespace;
+        }
+    };
+}
+
 const argocd_ns = new k8s.core.v1.Namespace("argo-cd-ns", {
     metadata: { name: "argo-cd" },
 });
@@ -14,11 +22,3 @@ const apps = new k8s.yaml.ConfigGroup("apps", {
     files: "apps/*.yaml",
     transformations: [useNamespace(argocd_ns)],
 });
-
-function useNamespace(namespace: Namespace) {
-    return (obj: any) => {
-        if (obj.metadata) {
-            obj.metadata.namespace = namespace;
-        }
-    };
-}
