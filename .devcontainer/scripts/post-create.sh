8e20d0c28cb5a389e0af1b3ab7b4775a641d6daf
Audacious Tux <tangimhossain1@gmail.com>
2023-07-06 00:57:14 +0600

    Revert "remove minikube"
    This reverts commit 6834e18472d4716cbd6f4d765eb3266f1271b049.

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -2,43 +2,43 @@
 
 set -eax
 
-# # clean up any untracked files
-# minikube delete
-# # start minikube
-# CPU_COUNT=4
-# MEMORY=6144
-# minikube start \
-#     --cpus=$CPU_COUNT \
-#     --memory=$MEMORY 
-# # use minikube's docker daemon
-# echo "eval \$(minikube docker-env)" >> ~/.zshrc
-# 
-# # if no backend url is specified
-# if [[ -z "$PULUMI_BACKEND_URL" ]]; then
-#     # pulumi cloud is the default, but if no access token is specified
-#     if [[ -z "$PULUMI_ACCESS_TOKEN" ]]; then
-#         # use local backend
-#         export PULUMI_BACKEND_URL="file://~"
-#         # if no passphrase is specified
-#         if [[ -z "$PULUMI_CONFIG_PASSPHRASE" ]]; then
-#             # use default passphrase
-#             export PULUMI_CONFIG_PASSPHRASE="pulumi"
-#             echo "export PULUMI_CONFIG_PASSPHRASE=$PULUMI_CONFIG_PASSPHRASE" >> ~/.zshrc
-#         fi
-#     fi
-# fi
-# 
-# pulumi login
-# 
-# # set default org if specified
-# if [[ -n "$PULUMI_DEFAULT_ORG" ]]; then
-#     pulumi org set-default $PULUMI_DEFAULT_ORG
-# fi
-# 
-# # set stack name to dev if not set
-# if [[ -z "$PULUMI_STACK_NAME" ]]; then
-#     export PULUMI_STACK_NAME="devcontainer"
-# fi
-# 
-# pulumi stack select -s $PULUMI_STACK_NAME --create -C platform
-# pulumi up -y --refresh -s $PULUMI_STACK_NAME --suppress-outputs -C platform
+# clean up any untracked files
+minikube delete
+# start minikube
+CPU_COUNT=4
+MEMORY=6144
+minikube start \
+    --cpus=$CPU_COUNT \
+    --memory=$MEMORY 
+# use minikube's docker daemon
+echo "eval \$(minikube docker-env)" >> ~/.zshrc
+
+# if no backend url is specified
+if [[ -z "$PULUMI_BACKEND_URL" ]]; then
+    # pulumi cloud is the default, but if no access token is specified
+    if [[ -z "$PULUMI_ACCESS_TOKEN" ]]; then
+        # use local backend
+        export PULUMI_BACKEND_URL="file://~"
+        # if no passphrase is specified
+        if [[ -z "$PULUMI_CONFIG_PASSPHRASE" ]]; then
+            # use default passphrase
+            export PULUMI_CONFIG_PASSPHRASE="pulumi"
+            echo "export PULUMI_CONFIG_PASSPHRASE=$PULUMI_CONFIG_PASSPHRASE" >> ~/.zshrc
+        fi
+    fi
+fi
+
+pulumi login
+
+# set default org if specified
+if [[ -n "$PULUMI_DEFAULT_ORG" ]]; then
+    pulumi org set-default $PULUMI_DEFAULT_ORG
+fi
+
+# set stack name to dev if not set
+if [[ -z "$PULUMI_STACK_NAME" ]]; then
+    export PULUMI_STACK_NAME="devcontainer"
+fi
+
+pulumi stack select -s $PULUMI_STACK_NAME --create -C platform
+pulumi up -y --refresh -s $PULUMI_STACK_NAME --suppress-outputs -C platform
